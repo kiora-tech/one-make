6b8b64f74f2714b88c475ca1c191023d18e9a441
Stephane Rathgeber <stephane@kiora.tech>
2023-05-09 19:43:17 +0200

    fix depedenci problem

--- one-make.sh
+++ one-make.sh
@@ -1,90 +1,80 @@
 #!/bin/bash
 
-# Déterminer le chemin vers la racine du projet
+# Determine the path to the project root
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PROJECT_ROOT="$SCRIPT_DIR/../../.."
 ONE_MAKE_PATH="vendor/kiora/one-make/make"
+LOCAL_MAKE_PATH="$PROJECT_ROOT/make"
 
-# Fonction pour récupérer les informations de l'utilisateur
+# Function to get user input
 get_user_input() {
-  read -p "Entrez l'IP du serveur de préproduction : " preprod_ip
-  read -p "Entrez le nom d'utilisateur du serveur de préproduction : " preprod_user
-  read -p "Entrez le nom d'utilisateur de la base de données : " db_user
-  read -p "Entrez l'hôte de la base de données : " db_host
-  read -p "Entrez le nom de la base de données : " db_name
-  read -p "Entrez le port de la base de données : " db_port
+  read -p "Enter the preproduction server IP: " preprod_ip
+  read -p "Enter the preproduction server username: " preprod_user
+  read -p "Enter the database username: " db_user
+  read -p "Enter the database host: " db_host
+  read -p "Enter the database name: " db_name
+  read -p "Enter the database port: " db_port
+
+  echo "PREPROD_IP=$preprod_ip" > "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "PREPROD_USER=$preprod_user" >> "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "DB_USER=$db_user" >> "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "DB_HOST=$db_host" >> "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "DB_NAME=$db_name" >> "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "DB_PORT=$db_port" >> "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "DB_TYPE=$db_type" >> "$LOCAL_MAKE_PATH/one-make.mk"
 }
 
-if [ -f "$PROJECT_ROOT/Makefile" ]; then
-  if grep -q "PREPROD_IP" "$PROJECT_ROOT/Makefile" && \
-     grep -q "PREPROD_USER" "$PROJECT_ROOT/Makefile" && \
-     grep -q "DB_USER" "$PROJECT_ROOT/Makefile" && \
-     grep -q "DB_HOST" "$PROJECT_ROOT/Makefile" && \
-     grep -q "DB_NAME" "$PROJECT_ROOT/Makefile" && \
-     grep -q "DB_PORT" "$PROJECT_ROOT/Makefile" && \
-     grep -q "DB_TYPE" "$PROJECT_ROOT/Makefile"; then
-    echo "Les informations requises sont déjà présentes dans le fichier Makefile."
-  else
-    get_user_input
-  fi
+
+# Create the 'make' directory if it doesn't exist
+if [ ! -d "$LOCAL_MAKE_PATH" ]; then
+  mkdir "$LOCAL_MAKE_PATH"
+fi
+
+if [ -f "$LOCAL_MAKE_PATH/one-make.mk" ]; then
+    echo "The required information is already present in the Makefile."
 else
+  # Search for the usage of MySQL or PostgreSQL in the .env file
+  db_type=""
+  if grep -q "DATABASE_URL=\"mysql" "$PROJECT_ROOT/.env"; then
+    db_type="mysql"
+  elif grep -q "DATABASE_URL=\"pgsql" "$PROJECT_ROOT/.env"; then
+    db_type="pgsql"
+  else
+    read -p "Are you using MySQL or PostgreSQL (mysql/pgsql)? " db_type
+  fi
+
   get_user_input
 fi
 
-# Recherche de l'utilisation de MySQL ou PostgreSQL dans le fichier .env
-db_type=""
-if grep -q "DATABASE_URL=mysql" "$PROJECT_ROOT/.env"; then
-  db_type="mysql"
-elif grep -q "DATABASE_URL=pgsql" "$PROJECT_ROOT/.env"; then
-  db_type="pgsql"
-else
-  read -p "Utilisez-vous MySQL ou PostgreSQL (mysql/pgsql) ? " db_type
-fi
 
-echo "PHP = php" >> "$PROJECT_ROOT/Makefile"
 
-# Vérification de l'utilisation de Docker
-use_docker=false
+# Check if Docker is being used
 if [ -f "$PROJECT_ROOT/Dockerfile" ] || [ -f "$PROJECT_ROOT/docker-compose.yml" ]; then
-  use_docker=true
+  cp -n "$ONE_MAKE_PATH/docker.mk" "$LOCAL_MAKE_PATH/"
 fi
 
-# Génération du fichier Makefile
-echo "PREPROD_IP=$preprod_ip" > "$PROJECT_ROOT/Makefile"
-echo "PREPROD_USER=$preprod_user" >> "$PROJECT_ROOT/Makefile"
-echo "DB_USER=$db_user" >> "$PROJECT_ROOT/Makefile"
-echo "DB_HOST=$db_host" >> "$PROJECT_ROOT/Makefile"
-echo "DB_NAME=$db_name" >> "$PROJECT_ROOT/Makefile"
-echo "DB_PORT=$db_port" >> "$PROJECT_ROOT/Makefile"
-echo "DB_TYPE=$db_type" >> "$PROJECT_ROOT/Makefile"
+if [ ! -z "$db_type" ]; then
+  cp -n "$ONE_MAKE_PATH/$db_type.mk" "$LOCAL_MAKE_PATH/"
+fi
+cp -n "$ONE_MAKE_PATH/help.mk" "$LOCAL_MAKE_PATH/"
+cp -n "$ONE_MAKE_PATH/symfony.mk" "$LOCAL_MAKE_PATH/"
 
-
-if ! grep -q "include $ONE_MAKE_PATH/$db_type.mk" "$PROJECT_ROOT/Makefile"; then
-  echo "include $ONE_MAKE_PATH/$db_type.mk" >> "$PROJECT_ROOT/Makefile"
-fi
-
-if [ "$use_docker" = true ] && ! grep -q "include $ONE_MAKE_PATH/docker.mk" "$PROJECT_ROOT/Makefile"; then
-  echo "include $ONE_MAKE_PATH/docker.mk" >> "$PROJECT_ROOT/Makefile"
-fi
-if ! grep -q "include $ONE_MAKE_PATH/help.mk" "$PROJECT_ROOT/Makefile"; then
-  echo "include $ONE_MAKE_PATH/help.mk" >> "$PROJECT_ROOT/Makefile"
-fi
-if ! grep -q "include $ONE_MAKE_PATH/symfony.mk" "$PROJECT_ROOT/Makefile"; then
-  echo "include $ONE_MAKE_PATH/symfony.mk" >> "$PROJECT_ROOT/Makefile"
-fi
 
 # Check if PHPUnit is installed
-if grep -q "\"phpunit/phpunit\"" "$PROJECT_ROOT/composer.json" && \
-   ! grep -q "include $ONE_MAKE_PATH/test.mk" "$PROJECT_ROOT/Makefile"; then
-  echo "include $ONE_MAKE_PATH/test.mk" >> "$PROJECT_ROOT/Makefile"
+if grep -q "\"phpunit/phpunit\"" "$PROJECT_ROOT/composer.json"; then
+  cp -n "$ONE_MAKE_PATH/tests.mk" "$LOCAL_MAKE_PATH/"
 fi
 
 
-if [ -f "$PROJECT_ROOT/package.json" ] && \
-   ! grep -q "include $ONE_MAKE_PATH/node.mk" "$PROJECT_ROOT/Makefile"; then
-  echo "include $ONE_MAKE_PATH/node.mk" >> "$PROJECT_ROOT/Makefile"
+if [ -f "$PROJECT_ROOT/package.json" ]; then
+  cp -n "$ONE_MAKE_PATH/node.mk" "$LOCAL_MAKE_PATH/"
 fi
 
-echo ".DEFAULT_GOAL:=help" >> "$PROJECT_ROOT/Makefile"
 
-echo "Makefile généré avec succès !"
+# Add "include make/*.mk" only if it's not already present in the Makefile
+if ! grep -q "include make/\*.mk" "$PROJECT_ROOT/Makefile"; then
+  echo ".DEFAULT_GOAL:=help" >> "$LOCAL_MAKE_PATH/one-make.mk"
+  echo "include make/*.mk" >> "$PROJECT_ROOT/Makefile"
+fi
+
+echo "Successfully generated Makefile !"
