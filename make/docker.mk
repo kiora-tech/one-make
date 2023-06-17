da0befb45e7bb86f5aa71a81fc03944fc321b1fd
Stephane Rathgeber <stephane@kiora.tech>
2023-06-17 14:35:42 +0200

    add docker tricks (sentinel) remove database befort import

--- make/docker.mk
+++ make/docker.mk
@@ -2,15 +2,21 @@ DOCKER_CMD = DOCKER_BUILDKIT=0 docker
 PHP = docker compose exec -it php
 JS_DEP = docker compose run --rm node yarn
 
+DOCKERFILES_FOLDER ?= .docker
+DOCKERFILES ?= $(wildcard $(DOCKERFILES_FOLDER)/Dockerfile.*)
+DOCKER_SENTINELS=$(addprefix make/, $(addsuffix .sentinel, $(subst Dockerfile.,.,$(notdir $(DOCKERFILES)))))
+
 ##@ Docker
-init: ## init docker
-	${DOCKER_CMD} compose build
+init: ${DOCKER_SENTINELS} ## init docker
 	${DOCKER_CMD} compose up -d
 
-up: ## run docker
-	${DOCKER_CMD} compose up -d
+$(DOCKER_SENTINELS): make/.%.sentinel : $(DOCKERFILES_FOLDER)/Dockerfile.%
+	${DOCKER_CMD} compose build
+	touch $@
+
+up: init ## run docker
+
+.PHONY: init up
 
 php: up ## run php container
 	${DOCKER_CMD} compose exec php bash
-
-.PHONY: init up php
--- make/mysql.mk
+++ make/mysql.mk
@@ -9,7 +9,7 @@ restore-db: backup.sql ## Restore database
 	cat backup.sql | $(MYSQL) $(DB_NAME)
 
 backup.sql:
-	ssh $(PREPROD_USER)@$(PREPROD_IP) '$(MYSQLDUMP) -u $(DB_USER) -p $(DB_NAME) > backup.sql'
+	ssh $(PREPROD_USER)@$(PREPROD_IP) '$(MYSQLDUMP) --add-drop-table -u $(DB_USER) -p $(DB_NAME) > backup.sql'
 	scp $(PREPROD_USER)@$(PREPROD_IP):backup.sql .
 
 .PHONY: load-db restore-db
